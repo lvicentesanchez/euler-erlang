65fdc4c9f34ebc4f852d12530128997024cf0109
Luis Angel Vicente Sanchez <langel.vicente@gmail.com>
2015-11-22 23:41:35 +0000

    * minor refactoring.

--- euler.erl
+++ euler.erl
@@ -3,15 +3,19 @@
 
 % Problem 1 from Euler project
 %
-problem1(N) -> lists:sum([X || X <- lists:seq(1, N-1), (X rem 3 == 0) or (X rem 5 == 0)]).
+problem1(N) ->
+    lists:sum([X || X <- lists:seq(1, N-1), (X rem 3 =:= 0) or (X rem 5 =:= 0)]).
 
 % Problem 2 from Euler project
 %
-problem2(Limit) -> problem2(Limit, 1, 2, 0, fun(N) -> N rem 2 == 0 end).
+problem2(Lim) ->
+    problem2(Lim, 1, 2, 0, fun(N) -> N rem 2 =:= 0 end).
 
-problem2(Limit,  _, N1, Sum, _) when N1 >= Limit-> Sum;
-problem2(Limit, N0, N1, Sum, F) ->
-  case F(N1) of
-    true  -> problem2(Limit, N1, N0+N1, Sum+N1, F);
-    false -> problem2(Limit, N1, N0+N1, Sum   , F)
-  end.
+problem2(Lim,  _, N1, Sum, _) when N1 >= Lim -> Sum;
+problem2(Lim, N0, N1, Sum, F)                ->
+    N2 = N0 + N1,
+    case F(N1) of
+        true  -> problem2(Lim, N1, N2, Sum+N1, F);
+        false -> problem2(Lim, N1, N2, Sum   , F)
+    end.
+
